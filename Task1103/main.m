60b7a2edf955e89eba9d6bfcc36188a93f73f512
Milan Manwar <milan.manwar@gmail.com>
2014-03-11 10:20:31 +0530

    Initial Commit

---
+++ Task1103/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Task1103
+//
+//  Created by Omnipresent on 11/03/14.
+//  Copyright (c) 2014 iOSCuriosity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Task1103Tests/Task1103Tests.m
@@ -0,0 +1,34 @@
+//
+//  Task1103Tests.m
+//  Task1103Tests
+//
+//  Created by Omnipresent on 11/03/14.
+//  Copyright (c) 2014 iOSCuriosity. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Task1103Tests : XCTestCase
+
+@end
+
+@implementation Task1103Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
